98254ecc0a27f66b18e4f758f1ade81973799f8c
Nick Harris <nicholas.edward.harris@gmail.com>
2020-06-15 14:09:06 +0700

    forgot one

---
+++ football_tg_bot.py
@@ -0,0 +1,44 @@
+# football_tg_bot.py
+'''
+This part is done
+'''
+from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, CallbackContext
+from football_postgres import FootballPostgresql
+import requests
+
+TG_BOT_TOKEN = '1241564289:AAGHB2cruHbI9QzOGzNft6UaDy3PkU34y_k'
+fpsql = FootballPostgresql()
+
+
+def command_handler_news(update, context):
+    text = get_news(update.callback_query.data)
+    chat_id = update.callback_query.message.chat_id
+    context.bot.send_message(chat_id, text='Testing')
+
+
+def news_callback_query_handler(update, context):
+    cqd = update.callback_query.data
+    news = fpsql.read_news(cqd)
+    chat_id = update.callback_query.message.chat_id
+    API_HOST = 'https://api.telegram.org'
+    path = f'/bot{TG_BOT_TOKEN}/sendMessage'
+    uri = API_HOST + path
+    params = {
+        'chat_id': chat_id,
+        'text': news,
+        'parse_mode': 'MarkdownV2',
+    }
+    requests.get(uri, params=params)
+    #bot.send_message(chat_id, text=news, parse_mode='MarkdownV2')
+
+
+def main():
+    updater = Updater(TG_BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CallbackQueryHandler(news_callback_query_handler))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
