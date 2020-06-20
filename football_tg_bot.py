7ee413f8f3a5903dde40ab413f414ffb5b206d1a
Nick Harris <nicholas.edward.harris@gmail.com>
2020-06-20 08:49:18 +0700

    added some logging to debug why it sent so many

--- football_tg_bot.py
+++ football_tg_bot.py
@@ -8,15 +8,17 @@ from football_postgres import FootballPostgresql
 import requests
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
 TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
 fpsql = FootballPostgresql()
+logging.basicConfig(filename='tg_bot.log', level=logging.DEBUG)
 
 
 def news_callback_query_handler(update, context):
     cqd = update.callback_query.data
-    print(cqd)
+    logging.info(f'news callback for cqd {cqd}')
     news = fpsql.read_news(cqd)
     chat_id = update.callback_query.message.chat_id
     API_HOST = 'https://api.telegram.org'
@@ -28,6 +30,7 @@ def news_callback_query_handler(update, context):
         'parse_mode': 'MarkdownV2',
     }
     requests.get(uri, params=params)
+    logging.info(f'sent request for cqd {cqd}')
 
 
 def main():
